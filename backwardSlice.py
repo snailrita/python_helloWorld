6ffac985918cd694d6154a45bf13319c893c5b94
snailrita <snailmailrita@gmail.com>
2020-09-14 07:05:43 +0100

    Going through section 3

---
+++ backwardSlice.py
@@ -0,0 +1,15 @@
+letters = "abcdefjhijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+
+qpo = letters[16:13:-1]
+print(qpo)
+
+print(letters[4::-1])
+print(letters[:-9:-1])
+
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
---
+++ repfields.py
@@ -0,0 +1,6 @@
+age = 24
+print("My age is {0} years".format(age))
+
+print("There are {0} days in {1}, {2}, {2}, {4}, {5}, {6} and {7}"
+      .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))
+print("There are {0}")
